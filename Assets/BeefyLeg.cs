0220b9f929d4274985e53248401fc8f421db105e
John Hiner <Jrwenceslaus@gmail.com>
2016-10-13 22:49:22 -0400

    It's coming together. Add an initial boost to toque on Left/Right arrow press

--- Assets/BeefyLeg.cs
+++ Assets/BeefyLeg.cs
@@ -8,6 +8,9 @@ public class BeefyLeg : MonoBehaviour {
     public Rigidbody2D myHips;
     public HingeJoint2D myKnee;
 
+    public HingeJoint2D myLeftShoulder;
+    public HingeJoint2D myRightShoulder;
+
     public AudioSource sound;
     public AudioClip jumpSound;
     public AudioClip hitSound;
@@ -23,6 +26,7 @@ public class BeefyLeg : MonoBehaviour {
 
     public Vector2 jumpForce;
     public float jForceMultiplier;
+    public float initJForceMultiplier;
     public Vector2 storedForce;
     public float maxStoredForce = 2000f;
 
@@ -51,7 +55,7 @@ public class BeefyLeg : MonoBehaviour {
         leftScale = transform.localScale;
         rightScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
 
-        jumpForce = Vector2.left + Vector2.up * jForceMultiplier * 10;
+        jumpForce = Vector2.up * jForceMultiplier * initJForceMultiplier;
         storedForce = jumpForce;    
     }
 	
@@ -61,14 +65,34 @@ public class BeefyLeg : MonoBehaviour {
         {
             SceneManager.LoadScene(0);
         }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             myBody.AddTorque(bodyTorque);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                myRightShoulder.useMotor = true;
+            }
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             myBody.AddTorque(-bodyTorque);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                myLeftShoulder.useMotor = true;
+            }
         }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            myBody.angularVelocity = 0;// AddTorque(bodyTorque);
+            myRightShoulder.useMotor = false;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            myBody.angularVelocity = 0;// AddTorque(-bodyTorque);
+            myLeftShoulder.useMotor = false;
+        }
+
         if (Input.GetKey(KeyCode.Space))
         {
             if (storedForce.y <= maxStoredForce)
@@ -79,7 +103,7 @@ public class BeefyLeg : MonoBehaviour {
             myBody.AddRelativeForce(storedForce, ForceMode2D.Force);
             myCalf.AddTorque(footTorque);
             myHips.AddTorque(-footTorque);
-            storedForce = jumpForce = Vector2.left + Vector2.up * jForceMultiplier * 10;
+            storedForce = jumpForce = Vector2.up * jForceMultiplier * initJForceMultiplier;
             sound.PlayOneShot(jumpSound,1f);
         }
     }
